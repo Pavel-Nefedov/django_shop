8cdd3b4b51c3e02f8bcb5021e966f6808b2a1783
Schooler <pavel.nefyodov.74@mail.ru>
2022-02-15 20:11:37 +0500

    add lesson_2 Вроде все сделал кроме задания под звездочкой, если что то забыл сделайте замечание, я доделаю

--- geekshop/geekshop/views.py
+++ geekshop/geekshop/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    title = 'GeekShop'
+    context = {
+        'title': title,
+    }
+    return render(request, 'geekshop/index.html', context)
+
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    title = 'контакты'
+    context = {
+        'title': title,
+    }
+    return render(request, 'geekshop/contacts.html', context)
