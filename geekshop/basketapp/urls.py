8ad5f221b960fff26b6b646c09a18966feb89147
Schooler <pavel.nefyodov.74@mail.ru>
2022-03-02 14:24:56 +0500

    add lesson_6

--- geekshop/basketapp/urls.py
+++ geekshop/basketapp/urls.py
@@ -6,6 +6,7 @@ app_name = 'basketapp'
 urlpatterns = [
     path('', basketapp.basket, name='index'),
     path('add/<int:pk>/', basketapp.basket_add, name='add'),
-    path('remove/<int:pk>)/', basketapp.basket_remove, name='remove'),
+    path('remove/<int:pk>/', basketapp.basket_remove, name='remove'),
+    path('edit/<int:pk>/<int:quantiti>/', basketapp.basket_edit, name='edit')
 ]
 
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,43 +1,77 @@
+import random
 from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
 from basketapp.models import Basket
 from mainapp.models import ProductCategory, Product
 
 
-#link_menu заменил на categories
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+
+def get_hot_product():
+    products = Product.objects.all()
+
+    return random.sample(list(products), 1)[0]
+
+
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)
+    return same_products
+
+
 def products(request, pk=None):
     title = 'каталог'
     categories = ProductCategory.objects.all()
-    basket = []
-    if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
+    basket = get_basket(request.user)
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
+    products = Product.objects.all().order_by('price')
 
     if pk is not None:
         if pk == 0:
             products = Product.objects.all().order_by('price')
-            category = {'name' : 'Все'}
+            category = {'name': 'Все'}
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
             products = Product.objects.filter(category__pk=pk).order_by('price')
 
         context = {
             'title': title,
-            'categories' : categories,
-            'products' : products,
-            'category' : category,
-            'basket': basket,
+            'categories': categories,
+            'products': products,
+            'category': category,
+            'basket': get_basket(request.user),
+            'hot_product': hot_product,
+            'same_products': same_products,
         }
 
         return render(request, 'mainapp/products.html', context)
 
-    same_products = Product.objects.all()
 
     context = {
         'title': title,
         'categories': categories,
-        'products': same_products,
+        'hot_product': hot_product,
+        'same_products': same_products,
         'basket': basket,
+        'products': products,
     }
 
     return render(request, 'mainapp/products.html', context)
+
+
+def product(request, pk):
+    title = 'продукты'
+
+    content = {
+        'title': title,
+        'links_menu': ProductCategory.objects.all(),
+        'product': get_object_or_404(Product, pk=pk),
+        'basket': get_basket(request.user),
+    }
+
+    return render(request, 'mainapp/product_detail.html', content)
