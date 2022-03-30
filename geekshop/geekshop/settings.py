4a52817ee89e8ed1bfd2f9fe7ca70d06381f1bc6
pashka <pavel.nefyodov.74@mail.ru>
2022-03-30 12:33:47 +0500

    add lesson_2.5 update settings

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -152,9 +152,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'geekshop', 'static'),
-)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'geekshop', 'static'),
+# )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
